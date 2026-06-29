bd5df24098afd144f3dd724453e01bef7f78d106
RoseGit <ing.rosalio.garcia.cruz@gmail.com>
2026-06-29 16:52:50 -0600

    Compare numbers

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std:: cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
@@ -33,7 +34,15 @@ fn main() {
     */
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     /*Imprimimos en terminal el valor que dio entrada el usuario.*/
     println!("You guessed: {guess}", );
+
+    match guess.cmp(&secret_number){
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 
 }
